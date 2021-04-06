39e6c7fae604d24444d911f21f09bb56ff94672a
PURPUSHECHKA <purpushechka@gmail.com>
2021-04-06 19:32:21 +0500

    update server request

--- client/components/mainWindowChat/viewMessage/ViewMessageOtherUsers.js
+++ client/components/mainWindowChat/viewMessage/ViewMessageOtherUsers.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const OtherUsers = ({ objMessage }) => {
   return (
-    <div className="flex justify-start p-3 w-3/4 rounded-lg mr-auto">
+    <div className="flex flex-col justify-start p-3 w-3/4 rounded-lg mr-auto">
       <div className="flex flex-row items-center">
         <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
           A
