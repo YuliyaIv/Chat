5ab8c7a359274005da721922725bc32bbe2c8f78
YuliyaIv <ranokgps@gmail.com>
2021-05-01 21:03:33 +0200

    added get request

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -14,3 +14,18 @@ exports.createUser = async (req, res) => {
     console.error(new Error(err))
   }
 }
+
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find()
+    res.status(200).json({
+      status: 'success',
+      data: {
+        users
+      }
+    })
+    res.send(users)
+  } catch (err) {
+    console.error(new Error(err))
+  }
+}
