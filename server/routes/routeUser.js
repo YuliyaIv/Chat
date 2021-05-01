c59c501a14f9c14d4a3760cce45402c2c9d7bd22
YuliyaIv <ranokgps@gmail.com>
2021-05-01 21:07:46 +0200

    added get request

--- server/routes/routeUser.js
+++ server/routes/routeUser.js
@@ -4,6 +4,6 @@ import userController from '../controller/userController'
 
 const router = express.Router()
 
-router.route('/').post(userController.createUser)
+router.route('/').post(userController.createUser).get(userController.getUsers)
 
 module.exports = router
