8926610968820ec96fbfe036db03f1c0c5d6ac8b
YuliyaIv <ranokgps@gmail.com>
2021-05-14 09:52:06 +0200

    created component PrivateRout.js

---
+++ client/components/PrivateRout.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import { useDispatch, useSelector } from 'react-redux'
+
+const PrivateComponent = () => {
+  return <div>private</div>
+}
+
+export default PrivateComponent
