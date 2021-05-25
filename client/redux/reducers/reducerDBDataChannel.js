ad61b34a7599cb7b965d46804ac6d03b6a4e6a24
YuliyaIv <ranokgps@gmail.com>
2021-05-26 00:28:33 +0200

    created reducer

--- client/redux/reducers/reducerDBDataChannel.js
+++ client/redux/reducers/reducerDBDataChannel.js
@@ -1,1 +1,23 @@
-const CREATE_CHANNEL = 'CREATE_CHANNEL'
+const GET_CHANNEL_DATA_DB = 'GET_CHANNEL_DATA_DB'
+
+const initialState = {
+  channels: null
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GET_CHANNEL_DATA_DB: {
+      return {
+        ...state,
+        channels: action.channels
+      }
+    }
+    default:
+      return state
+  }
+}
+
+export function getChannelsDataDb(channelsID) {
+  console.log('channelsID', channelsID)
+  return { type: GET_CHANNEL_DATA_DB, channels: channelsID }
+}
