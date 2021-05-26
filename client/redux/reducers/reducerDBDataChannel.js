8e2a1f0c0ef3275e9146b3872503aa67fa4d53e8
YuliyaIv <ranokgps@gmail.com>
2021-05-26 16:01:55 +0200

    added action creater getParticularChannelDb

--- client/redux/reducers/reducerDBDataChannel.js
+++ client/redux/reducers/reducerDBDataChannel.js
@@ -1,15 +1,27 @@
-const GET_CHANNEL_DATA_DB = 'GET_CHANNEL_DATA_DB'
+import axios from 'axios'
+
+const GET_CHANNELS_DATA_DB = 'GET_CHANNELS_DATA_DB'
+const GET_DATA_PARTICULAR_CHANNEL = 'GET_DATA_PARTICULAR_CHANNEL'
 
 const initialState = {
-  channels: null
+  channels: null,
+  particularChannelId: null,
+  particularChannelData: null
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case GET_CHANNEL_DATA_DB: {
+    case GET_CHANNELS_DATA_DB: {
       return {
         ...state,
         channels: action.channels
+      }
+    }
+    case GET_DATA_PARTICULAR_CHANNEL: {
+      return {
+        ...state,
+        particularChannelData: action.particularChannelData,
+        particularChannelId: action.particularChannelId
       }
     }
     default:
@@ -18,6 +30,37 @@ export default (state = initialState, action) => {
 }
 
 export function getChannelsDataDb(channelsID) {
-  console.log('channelsID', channelsID)
-  return { type: GET_CHANNEL_DATA_DB, channels: channelsID }
+  return { type: GET_CHANNELS_DATA_DB, channels: channelsID }
 }
+
+export function getParticularChannelDb(channelId) {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios({
+        method: 'get',
+        url: `/api/v2/channel/${channelId}`
+      })
+      dispatch({
+        type: GET_DATA_PARTICULAR_CHANNEL,
+        particularChannelData: data.data.dataName,
+        particularChannelId: channelId
+      })
+      console.log(data)
+    } catch (err) {
+      console.error(new Error(err), 'error send new message')
+    }
+  }
+}
+
+// export function getDataParticularChannel(idChannel) {
+//   return (dispatch, getState) => {
+//     const store = getState()
+//     const dataParticularChannel = store.reducerDataChannels.dataChannels[idChannel]
+
+//     dispatch({
+//       type: REDUSER_DATA_PARTICULAR_CHANNEL,
+//       dataParticularChannel,
+//       dataParticularId: idChannel
+//     })
+//   }
+// }
