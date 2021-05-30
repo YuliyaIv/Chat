6277e2d9c39bd26a79bbc61cd99d392295471952
YuliyaIv <ranokgps@gmail.com>
2021-05-30 23:40:31 +0200

    added object for permission for virtuals data

--- server/modelsDB/messageModel.js
+++ server/modelsDB/messageModel.js
@@ -1,25 +1,33 @@
 import mongoose from 'mongoose'
 
-const messageSchema = new mongoose.Schema({
-  timeCreateMessage: {
-    type: String // must be  Date
+const messageSchema = new mongoose.Schema(
+  {
+    timeCreateMessage: {
+      type: String, // must be  Date
+      default: 'now'
+    },
+    timeDeleteMessage: {
+      type: String // must be  Date
+    },
+    idUserPostedMessage: {
+      type: String
+    },
+    textMessage: {
+      type: String,
+      maxlength: [1500, 'The text message must have less or equal then 1500 characters'],
+      minlength: [1, 'The text message must have more or equal then 1 characters'],
+      trim: true
+    },
+    channelOvner: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Channel'
+    }
   },
-  timeDeleteMessage: {
-    type: String // must be  Date
-  },
-  idMessage: {
-    type: Number // delete after,
-  },
-  idUserPostedMessage: {
-    type: String
-  },
-  textMessage: {
-    type: String,
-    maxlength: [1500, 'The text message must have less or equal then 1500 characters'],
-    minlength: [1, 'The text message must have more or equal then 1 characters'],
-    trim: true
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
   }
-})
+)
 
 const Message = mongoose.model('Message', messageSchema)
 
