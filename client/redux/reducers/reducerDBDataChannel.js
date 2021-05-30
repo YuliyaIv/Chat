074f97c04a0d4ddf144af8834422b61522c00620
YuliyaIv <ranokgps@gmail.com>
2021-05-30 23:36:15 +0200

    added new actions creator for new message and for get particular channel from db

--- client/redux/reducers/reducerDBDataChannel.js
+++ client/redux/reducers/reducerDBDataChannel.js
@@ -2,11 +2,13 @@ import axios from 'axios'
 
 const GET_CHANNELS_DATA_DB = 'GET_CHANNELS_DATA_DB'
 const GET_DATA_PARTICULAR_CHANNEL = 'GET_DATA_PARTICULAR_CHANNEL'
+const SET_NEW_MESSAGE = 'SET_NEW_MESSAGE'
 
 const initialState = {
   channels: null,
   particularChannelId: null,
-  particularChannelData: null
+  particularChannelData: null,
+  newMessage: null
 }
 
 export default (state = initialState, action) => {
@@ -24,6 +26,13 @@ export default (state = initialState, action) => {
         particularChannelId: action.particularChannelId
       }
     }
+    case SET_NEW_MESSAGE: {
+      return {
+        ...state,
+        particularChannelData: action.particularChannelData,
+        newMessage: action.newMessage
+      }
+    }
     default:
       return state
   }
@@ -33,34 +42,61 @@ export function getChannelsDataDb(channelsID) {
   return { type: GET_CHANNELS_DATA_DB, channels: channelsID }
 }
 
+/// work not good, strange rerender and close shell modal window
+// export function getParticularChannelDb(channelId) {
+//   return async (dispatch) => {
+//     try {
+//       const { data } = await axios({
+//         method: 'get',
+//         url: `/api/v2/channel/${channelId}`
+//       })
+//       dispatch({
+//         type: GET_DATA_PARTICULAR_CHANNEL,
+//         particularChannelData: data.data.dataName,
+//         particularChannelId: channelId
+//       })
+//       console.log(data)
+//     } catch (err) {
+//       console.error(new Error(err), 'error send new message')
+//     }
+//   }
+// }
+
 export function getParticularChannelDb(channelId) {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
     try {
-      const { data } = await axios({
-        method: 'get',
-        url: `/api/v2/channel/${channelId}`
-      })
+      const store = getState()
+      const dataParticularChannel = store.reducerDBDataChannel.channels.find(
+        (channel) => channel._id === channelId
+      )
       dispatch({
         type: GET_DATA_PARTICULAR_CHANNEL,
-        particularChannelData: data.data.dataName,
+        particularChannelData: dataParticularChannel,
         particularChannelId: channelId
       })
-      console.log(data)
     } catch (err) {
-      console.error(new Error(err), 'error send new message')
+      console.error(new Error(err), 'error:  no channel ')
     }
   }
 }
 
-// export function getDataParticularChannel(idChannel) {
-//   return (dispatch, getState) => {
-//     const store = getState()
-//     const dataParticularChannel = store.reducerDataChannels.dataChannels[idChannel]
+export function setNewMessageDB(textMessage, idChannel, idUser) {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.post('/api/v2/message', {
+        textMessage,
+        channelOvner: idChannel,
+        idUserPostedMessage: idUser
+      })
+      console.log('data.updatedChannel', data)
 
-//     dispatch({
-//       type: REDUSER_DATA_PARTICULAR_CHANNEL,
-//       dataParticularChannel,
-//       dataParticularId: idChannel
-//     })
-//   }
-// }
+      dispatch({
+        type: SET_NEW_MESSAGE,
+        particularChannelData: data.data.updatedChannel,
+        newMessage: data.data.newMessage
+      })
+    } catch (err) {
+      console.error(new Error(err), 'error: send new message')
+    }
+  }
+}
