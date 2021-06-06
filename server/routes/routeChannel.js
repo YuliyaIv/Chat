d656e9227741fd12426601d328644a53e5a3a674
YuliyaIv <ranokgps@gmail.com>
2021-06-06 15:53:27 +0200

    deleted old route for delete message

--- server/routes/routeChannel.js
+++ server/routes/routeChannel.js
@@ -8,18 +8,11 @@ router.route('/').post(channelsController.createChannel).get(channelsController.
 
 router.route('/channellist/:idUser').get(channelsController.getUserChannels)
 
-// need new method 'add message id'
 router
   .route('/:id')
   .get(channelsController.getChannel)
   .patch(channelsController.changeNameOrDescription)
   .delete(channelsController.deleteChannel)
   .post(channelsController.addIdMessage)
-
-// delete / have new schema and route for message
-router
-  .route('/:id/:idMess')
-  .patch(channelsController.changeMessage)
-  .delete(channelsController.deleteMessage)
 
 module.exports = router
