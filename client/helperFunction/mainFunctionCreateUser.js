8422c7227a53cbbe0b99d496242565f81dfdd5fb
YuliyaIv <ranokgps@gmail.com>
2021-07-10 16:02:45 +0200

    created a helper function for creating a user

---
+++ client/helperFunction/mainFunctionCreateUser.js
@@ -0,0 +1,12 @@
+const NewObjUser = function ({ name, password, confirmPassword, email }) {
+  this.role = ['user']
+  this.channelsAccess = []
+  this.channelsOvner = []
+  this.nameUser = name
+  this.avatar = 'default'
+  this.password = password
+  this.passwordConfirm = confirmPassword
+  this.email = email
+}
+
+export default NewObjUser
