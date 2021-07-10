b89b18a004af80ace0ff79992aea8db8958d963b
YuliyaIv <ranokgps@gmail.com>
2021-07-10 15:53:54 +0200

    components removed and replaced with a different structure

--- client/components/PrivateRout.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-// import { useDispatch, useSelector } from 'react-redux'
-
-const PrivateComponent = () => {
-  return <div>private</div>
-}
-
-export default PrivateComponent
