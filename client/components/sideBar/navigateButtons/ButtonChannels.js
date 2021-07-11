d63a17353e2134939c89e47ffb91a5825e44b540
YuliyaIv <ranokgps@gmail.com>
2021-07-11 13:12:35 +0200

    naming conflict ide and git on account of uppercase, deleting a component and creating with the same logic

---
+++ client/components/sideBar/navigateButtons/ButtonChannels.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { setFlagRenderSideBarView } from '../../../redux/reducers/reducerSetFlagRender'
+
+const ButtonChannels = () => {
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <button
+        type="button"
+        href="#"
+        className="focus:outline-none flex items-center"
+        onClick={() => dispatch(setFlagRenderSideBarView('showComponentChannels'))}
+      >
+        <span className="flex items-center justify-center text-cyan-100 hover:bg-cyan-700 h-12 w-12 rounded-2xl">
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+            />
+          </svg>
+        </span>
+      </button>
+    </div>
+  )
+}
+
+ButtonChannels.propType = {}
+
+export default React.memo(ButtonChannels)
